2354d0ff4d3f3284810d4259f41f0c26d572beaa
Wang Yibo <wangyibofgx@emails.bjut.edu.cn>
2024-07-02 14:12:10 +0800

    实现了初步的移动逻辑

--- src/behaviours/Walkable.ts
+++ src/behaviours/Walkable.ts
@@ -4,11 +4,48 @@ export class Walkable extends Behaviour {
   @number()
   speed = 1;
 
+  @number()
+  jumpForce = 5;
+
   onStart() {
-    this.gameObject.onClick = () => {
-      const rigid = this.gameObject.getBehaviour(RigidBody);
-      // rigid.b2RigidBody.SetLinearVelocity({ x: this.speed, y: 0 })
-      rigid.b2RigidBody.SetLinearVelocity({ x: this.speed, y: 0 });
-    };
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    document.addEventListener('keyup', this.handleKeyUp.bind(this));
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    const rigid = this.gameObject.getBehaviour(RigidBody);
+
+    switch (event.code) {
+      case 'ArrowLeft':
+        rigid.b2RigidBody.SetLinearVelocity({ x: -this.speed, y: 0 });
+        break;
+      case 'ArrowRight':
+        rigid.b2RigidBody.SetLinearVelocity({ x: this.speed, y: 0 });
+        break;
+      case 'KeyC':
+        rigid.b2RigidBody.SetLinearVelocity({ x: rigid.b2RigidBody.GetLinearVelocity().x, y: this.jumpForce });
+        break;
+    }
+  }
+
+  handleKeyUp(event: KeyboardEvent) {
+    const rigid = this.gameObject.getBehaviour(RigidBody);
+
+    switch (event.code) {
+      case 'ArrowLeft':
+        rigid.b2RigidBody.SetLinearVelocity({ x: 0, y: 0 });
+        break;
+      case 'ArrowRight':
+        rigid.b2RigidBody.SetLinearVelocity({ x: 0, y: 0 });
+        break;
+      case 'KeyC':
+        break;
+    }
+  }
+
+  onDestroy() {
+    document.removeEventListener('keydown', this.handleKeyDown.bind(this));
+    document.removeEventListener('keyup', this.handleKeyUp.bind(this));
   }
 }
+
