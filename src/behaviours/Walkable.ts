de86ed177e8766e1ce04930fbdbaeac161c0d98a
Wang Yibo <wangyibofgx@emails.bjut.edu.cn>
2024-07-02 10:02:59 +0800

    更改了走路的逻辑

--- src/behaviours/Walkable.ts
+++ src/behaviours/Walkable.ts
@@ -4,11 +4,31 @@ export class Walkable extends Behaviour {
   @number()
   speed = 1;
 
+  @number()
+  jumpForce = 5;
+
+  private rigid: RigidBody;
+
   onStart() {
-    this.gameObject.onClick = () => {
-      const rigid = this.gameObject.getBehaviour(RigidBody);
-      // rigid.b2RigidBody.SetLinearVelocity({ x: this.speed, y: 0 })
-      rigid.b2RigidBody.SetLinearVelocity({ x: this.speed, y: 0 });
-    };
+    this.rigid = this.gameObject.getBehaviour(RigidBody);
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    switch (event.code) {
+      case 'ArrowLeft':
+        this.rigid.b2RigidBody.SetLinearVelocity({ x: -this.speed, y: this.rigid.b2RigidBody.GetLinearVelocity().y });
+        break;
+      case 'ArrowRight':
+        this.rigid.b2RigidBody.SetLinearVelocity({ x: this.speed, y: this.rigid.b2RigidBody.GetLinearVelocity().y });
+        break;
+      case 'KeyC':
+        this.rigid.b2RigidBody.SetLinearVelocity({ x: this.rigid.b2RigidBody.GetLinearVelocity().x, y: this.jumpForce });
+        break;
+    }
+  }
+
+  onDestroy() {
+    document.removeEventListener('keydown', this.handleKeyDown.bind(this));
   }
 }
